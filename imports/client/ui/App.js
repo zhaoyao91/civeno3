1fe624a27066e025d0a951772fa77514d08d7ebd
zhaoyao91 <zhaoyao91@163.com>
2017-04-28 00:49:01 +0800

    add routes and IndexPage

--- imports/client/ui/App.js
+++ imports/client/ui/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
-const App = () => (<div>
-  <h1>React App</h1>
-</div>)
+import IndexPage from './pages/IndexPage'
+
+// used to hold all global components and top level routes
+const App = () => (
+  <div>
+    <Router>
+      <Route exact path="/" component={IndexPage}/>
+    </Router>
+  </div>
+)
 
 export default App
---
+++ imports/client/ui/pages/IndexPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexPage = () => (
+  <div>
+    <h1>Index Page</h1>
+  </div>
+)
+
+export default IndexPage
