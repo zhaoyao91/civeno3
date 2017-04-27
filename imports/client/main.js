efa8324fb1da9a56785dc9b376e94f28e8b4b1ef
zhaoyao91 <zhaoyao91@163.com>
2017-04-28 00:23:07 +0800

    add react app entry

--- imports/client/main.js
+++ imports/client/main.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './ui/App'
+
+// import semantic styles
 import 'semantic-ui-css/semantic.min.css'
+
+// render react app
+ReactDOM.render(<App/>, document.getElementById('react-root'))
