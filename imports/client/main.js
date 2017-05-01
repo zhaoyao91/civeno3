530d4b014b65bad546f937db52b1d44b575aaa1a
zhaoyao91 <zhaoyao91@163.com>
2017-05-01 23:25:58 +0800

    load styles into css files to liverage browser cache

--- imports/client/main.js
+++ imports/client/main.js
@@ -2,9 +2,6 @@ import createBrowserHistory from 'history/createBrowserHistory'
 
 import renderApp from './boot/render_app'
 import configAccounts from './boot/config_accounts'
-import importStyles from './boot/import_styles'
-
-importStyles()
 
 const history = createBrowserHistory()
 
