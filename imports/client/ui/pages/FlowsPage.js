55d31532978bcae8ae8a5f93600f015319e29998
zhaoyao91 <zhaoyao91@163.com>
2017-05-02 12:15:55 +0800

    subscribe flows in flows page

--- imports/client/ui/pages/FlowsPage.js
+++ imports/client/ui/pages/FlowsPage.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
-import { flatten } from 'lodash/fp'
 import { compose, withState, withHandlers } from 'recompose'
+import { Meteor } from 'meteor/meteor'
+import { flatten } from 'lodash/fp'
 
+import withMeteorData from '../hocs/with_meteor_data'
+import Flows from '../../../common/collections/flows'
 import MainTopLayout from '../layouts/MainTopLayout'
 import CreateFlowModal from '../views/CreateFlowModal'
 
 const FlowsPage = () => (
   <MainTopLayout>
-    <CardsLayout>
-      <CreateFlowCard/>
-    </CardsLayout>
+    <FlowsView/>
   </MainTopLayout>
 )
 
@@ -19,7 +20,7 @@ export default FlowsPage
 const CardsLayout = ({children}) => (
   <div style={{height: '100%', overflow: 'auto', padding: '2rem 0 0 2rem'}}>
     {
-      flatten([children]).map((card, index) => (
+      children.map((card, index) => (
         <div key={index} style={{float: 'left', marginRight: '2rem', marginBottom: '2rem'}}>{card}</div>
       ))
     }
@@ -39,4 +40,47 @@ const CreateFlowCard = compose(
     </Card.Content>
     <CreateFlowModal open={modalVisible} onOpen={openModal} onClose={closeModal}/>
   </Card>
+))
+
+const FlowCard = compose(
+
+)(({flow}) => (
+  <Card style={{width: '300px', height: '170px'}}>
+    <Card.Content>
+      <Card.Header>{flow.name}</Card.Header>
+      <Card.Description>{flow.description}</Card.Description>
+    </Card.Content>
+  </Card>
+))
+
+const FlowsView = compose(
+  withMeteorData(() => ({userId: Meteor.userId()})),
+  withMeteorData(({userId}) => {
+    const sub = Meteor.subscribe('Flow.flowsOfOwner', userId)
+    return {
+      dataReady: sub.ready()
+    }
+  }),
+  withMeteorData(({userId, dataReady}) => {
+    if (userId && dataReady) {
+      return {
+        flows: Flows.find({owner: userId}).fetch()
+      }
+    } else {
+      return {
+        flows: []
+      }
+    }
+  })
+)(({flows}) => (
+  <CardsLayout>
+    {
+      flatten([
+        <CreateFlowCard key="0"/>,
+        flows.map(flow => (
+          <FlowCard key={flow._id} flow={flow}/>
+        ))
+      ])
+    }
+  </CardsLayout>
 ))
