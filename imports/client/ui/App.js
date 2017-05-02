afebb9833d8ce1d85afa011a4a07f919b9650152
zhaoyao91 <zhaoyao91@163.com>
2017-05-02 17:59:12 +0800

    add my profile page

--- imports/client/ui/App.js
+++ imports/client/ui/App.js
@@ -21,6 +21,7 @@ const FlowDefinitionPage = requireAuth(lazyLoad(() => import('./pages/FlowDefini
 const FlowInstancesPage = requireAuth(lazyLoad(() => import('./pages/FlowInstancesPage')))
 const FlowDataPage = requireAuth(lazyLoad(() => import('./pages/FlowDataPage')))
 const FlowApplicationsPage = requireAuth(lazyLoad(() => import('./pages/FlowApplicationsPage')))
+const MyProfilePage = requireAuth(lazyLoad(() => import('./pages/MyProfilePage')))
 
 // used to hold all global components and top level routes
 const App = ({history}) => (
@@ -32,6 +33,7 @@ const App = ({history}) => (
         <Route exact path="/login" component={LoginPage}/>
         <Route exact path="/forgot-password" component={ForgotPasswordPage}/>
         <Route exact path="/reset-password/:token" component={ResetPasswordPage}/>
+        <Route exact path="/my/profile" component={MyProfilePage}/>
         <Route exact path="/flows" component={FlowsPage}/>
         <Route exact path="/flow/:flowId" component={FlowPage}/>
         <Route exact path="/flow/:flowId/definition" component={FlowDefinitionPage}/>
---
+++ imports/client/ui/pages/MyProfilePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import MainNavBarLayout from '../layouts/MainNavBarLayout'
+import MyPageLayout from '../layouts/MyPageLayout'
+
+const MyProfilePage = () => (
+  <MainNavBarLayout>
+    <MyPageLayout>
+      my profile
+    </MyPageLayout>
+  </MainNavBarLayout>
+)
+
+export default MyProfilePage
