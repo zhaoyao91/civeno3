ebbfd0acf9fb905c14f85f89405dbf4b93d77872
zhaoyao91 <zhaoyao91@163.com>
2017-05-03 13:09:19 +0800

    cache user profile subscription

--- imports/client/ui/hocs/with_current_user.js
+++ imports/client/ui/hocs/with_current_user.js
@@ -1,10 +1,13 @@
 import { Meteor } from 'meteor/meteor'
+import { SubsCache } from 'meteor/ccorcos:subs-cache'
 
 import withMeteorData from './with_meteor_data'
 
+const subsCache = new SubsCache()
+
 export default withMeteorData(() => {
   if (Meteor.userId()) {
-    Meteor.subscribe('User.userProfile', Meteor.userId())
+    subsCache.subscribe('User.userProfile', Meteor.userId())
   }
   return {
     user: Meteor.user()
